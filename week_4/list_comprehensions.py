fc718d9435ae6036216f8267ccc70169761abb84
khan_pog <Khanthomps123@gmail.com>
2021-08-16 20:18:45 +1000

    add list comprehension to complete various tasks + need help with last task

--- week_4/list_comprehensions.py
+++ week_4/list_comprehensions.py
@@ -29,12 +29,14 @@ print(a_names)
 
 # TODO: use a list comprehension to create a list of all of the full_names
 # in lowercase format
-# lowercase_full_names =
-
+lowercase_full_names = [full_name for full_name in full_names]
+print(lowercase_full_names)
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: use a list comprehension to create a list of integers
 # from the above list of strings
-# numbers =
-
+numbers = [ str(almost_number) for almost_number in almost_numbers]
+print(numbers)
 # TODO: use a list comprehension to create a list of only the numbers that are
+numbers = [almost_number for almost_number in almost_numbers if almost_number.isdigit()]
+print(numbers)
 # greater than 9 from the numbers (not strings) you just created
