f64681229a13f1037108fcddf6a73fca54e6319c
Khan-pog <Elmo0747>
2021-08-21 16:22:58 +1000

    ADD #DONE: marking all complete practical exercises

--- week_3/broken_score.py
+++ week_3/broken_score.py
@@ -2,19 +2,33 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
 
-def main(): #DONE: ADDED TO MAIN
+def main():  # DONE: ADDED TO MAIN
+    """Function docstring"""
     score = float(input("Enter score: "))
-    if score <= 0:
-        print("Invalid score")
-    elif 0 < score < 50:
-        print("Bad")
-    elif 50 <= score < 90:
-        print("Passable")
-    elif 90 <= score <= 100:
-        print("Excellent")
+    result = processes_score_into_result(score)  # DONE: ADD FUNCTION WHERE OLD CODE WAS
+    print(result)
+    score = random.randint(0, 100)
+    processes_score_into_result(score)  # DONE: ADDED RANDOM SCORE TURNED THAT SCORE INTO A RESULT THEN PRINTED
+    print(result)
+
+def processes_score_into_result(score):  # DONE: MADE SCORE PROCESSES FUNCTION
+    """ processes a score into a result """
+    # statements...
+    if 0 < score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
     else:
-        print("Invalid score")
+        return "Excellent"
+
+
+
+
+
 
 main()
--- week_3/temperatures.py
+++ week_3/temperatures.py
@@ -6,23 +6,41 @@ Pseudocode for temperature conversion
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print("Result: {:.2f} F".format(celsius))
-        # TODO: Write this section to convert F to C and display the result
-        # Hint:
-        # Remove the "pass" statement when you are done. It's a placeholder.
-        pass
-    else:
-        print("Invalid option")
+
+
+def main():  # DONE: MOVED ALL INTO MAIN
+    """ take user input and output a tempter conversion """
+    # variables...
+
+    # statements...
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = converting_celsius_to_fahrenheit(celsius)  # DONE: ADD FUNCTION WHERE OLD CODE WAS
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = converting_fahrenheit_to_celsius(fahrenheit)  # DONE: ADD FUNCTION WHERE OLD CODE WAS
+            print("Result: {:.2f} F".format(celsius))
+        else:
+            print("Invalid option")
+    print(MENU)
+    choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def converting_celsius_to_fahrenheit(celsius):  # DONE: ADDED Converting_celsius_to_fahrenheit
+    """Turns celsius to fahrenheit. ouputs fahrenheit"""
+    # statements...
+    return celsius * 9.0 / 5 + 32
+
+
+def converting_fahrenheit_to_celsius(fahrenheit):  # DONE: ADDED converting_fahrenheit_to_celsius
+    """Turns fahrenheit into celsius. ouputs celsius"""
+    # statements...
+    return 5 / 9 * (fahrenheit - 32)
+
+
+main()
