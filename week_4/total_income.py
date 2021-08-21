4987b8c878c4c629240a53bc986e8f0216b04c23
Khan-pog <Elmo0747>
2021-08-21 16:57:30 +1000

    ADD #DONE: marking all complete practical exercises

--- week_4/total_income.py
+++ week_4/total_income.py
@@ -7,10 +7,10 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of number_of_months."""
     incomes = []
-    number_of_months = int(input("How many number_of_months? "))
+    number_of_months = int(input("How many number_of_months? "))   # DONE : Refactor the months variable to number_of_months
 
     for month in range(1, number_of_months + 1):
-        income = float(input("Enter income for month {} : ".format(str(month))))
+        income = float(input("Enter income for month {}: ".format(str(month))))  # DONE : Change the line that gets the income input so that it uses the string format() method instead of string concatenation (+).
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
