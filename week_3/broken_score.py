e328f8832aea6f20c595d99e23d30c6d98b1006c
Khan-pog <Elmo0747>
2021-08-21 15:52:11 +1000

    Copy files from week 1 to week 3

---
+++ week_3/broken_score.py
@@ -0,0 +1,20 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+
+def main(): #DONE: ADDED TO MAIN
+    score = float(input("Enter score: "))
+    if score <= 0:
+        print("Invalid score")
+    elif 0 < score < 50:
+        print("Bad")
+    elif 50 <= score < 90:
+        print("Passable")
+    elif 90 <= score <= 100:
+        print("Excellent")
+    else:
+        print("Invalid score")
+
+main()
