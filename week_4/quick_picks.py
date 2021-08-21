d8e04be9f3ec2049197bcf44e35fa4fddca80ce0
Khan-pog <Elmo0747>
2021-08-21 19:31:42 +1000

    ADD #DONE: marking all complete practical exercises

--- week_4/quick_picks.py
+++ week_4/quick_picks.py
@@ -1,14 +1,23 @@
-
-
 import random
 
-number_of_quick_picks = 3
-num = 0
-number_of_numbers_in_qickpick = 6
 
-for i in range(number_of_quick_picks):
-    qucik_pick = []
-    for number in range(number_of_numbers_in_qickpick):
-        qucik_pick.append(random.randint(1, 45))
-    print(f"{qucik_pick[0]:>2}{qucik_pick[1]:>3}{qucik_pick[2]:>3}{qucik_pick[3]:>3}{qucik_pick[4]:>3}{qucik_pick[5]:>3}")
+NUMBEROFRANDOMNUMBERS = 6 # DONE: STORED AS CONSTANTS
 
+QUICKPICKSMAXNUMBER = 45
+
+
+
+
+number_of_pick_picks = int(input("How many qick picks?"))
+
+for i in range(number_of_pick_picks):
+    qucik_pick = [0]*NUMBEROFRANDOMNUMBERS
+
+    for number in range(NUMBEROFRANDOMNUMBERS):
+        random_number = random.randint(1, QUICKPICKSMAXNUMBER)
+        while random_number == qucik_pick[number]:  # DONE:  EACH LINE (QUICK PICK) SHOULD NOT CONTAIN ANY REPEATED NUMBER.
+            random_number = random.randint(1, QUICKPICKSMAXNUMBER)
+        qucik_pick[number] = random_number
+    qucik_pick.sort()   # DONE:  EACH LINE OF NUMBERS SHOULD BE DISPLAYED IN SORTED (ASCENDING) ORDER.
+    print(
+        f"{qucik_pick[0]:>2}{qucik_pick[1]:>3}{qucik_pick[2]:>3}{qucik_pick[3]:>3}{qucik_pick[4]:>3}{qucik_pick[5]:>3}")
