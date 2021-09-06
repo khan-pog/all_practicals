9750de72b97fbb841c799a0e3776408078669e9f
Khan-pog <Elmo0747>
2021-09-06 10:59:42 +1000

    Complete modifying car.py and used_car.py.

--- practical_6/car.py
+++ practical_6/car.py
@@ -4,13 +4,21 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, fuel=0, name='car'): # Done.
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
         self.odometer = 0
+        self.name = name
+
+    def __str__(self):
+        """Print this string format when called."""
+        # Done.
+        return '{}, fuel={}, odometer={}'.format(str(self.name),
+                                                 str(self.fuel),
+                                                 str(self.odometer))
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
