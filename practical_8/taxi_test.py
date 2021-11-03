275291199368b48d3f71bbb1d81aaecb664854d8
khan-pog <62162976+khan-pog@users.noreply.github.com>
2021-11-03 11:48:26 +1100

    Implement tests of taxi

---
+++ practical_8/taxi_test.py
@@ -0,0 +1,12 @@
+"""
+CP1404/CP5632 Practical
+Car class, test
+"""
+from practical_8.taxi import Taxi
+
+car = Taxi("Prius 1", 100, 1.23)
+car.drive(40)
+print(car)
+car.start_fare()
+car.drive(100)
+print(car)
